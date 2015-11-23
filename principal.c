b4da086988452cc803f53e3ca6d05493d2c31735
NeftaliAcosta <neftaliacosta@outlook.com>
2015-11-23 11:55:58 -0600

    Archivos iniciales de punto de partida

---
+++ principal.c
@@ -0,0 +1,39 @@
+ /*
+Versión 0.1
+Autor: NEFTALI MARCIANO ACOSTA
+Universidad Abierta y a Distancia de México
+Materia:Fundamentos de programación
+Faclitiadora: Vanessa Avendaño Trejo
+ES1421008504@unadmexico.mx
+Fecha de cración: 14/Agost/15
+Ultima Actualización 25/09/15
+
+====CONTROLADOR DE VERSIONES====
+Versión 0.1
+============DESCRIPCION=========
+Función validadora de la conexión de la base de datos. En caso de no existir configuración
+establecerla en ese momento y gudarlar en bloc de notas.
+
+Una vez guardada la configuración, leer la configuración, establer conxión y abrir el menú principal.
+
+*/
+
+/*=========LIBRERÍAS NECESARIAS=========*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <mysql.h>
+#include <windows.h>
+
+
+
+int main(int argc, char *argv[]) {
+	SetConsoleTitle("Version 1");
+	system("pause");
+
+}
+
+
+
+
